c2c326ef70fdb15536a2252c5aa863fc908d9303
Prabhanshu Abhishek <prabhanshua@gmail.com>
2024-03-31 18:45:19 +0530

    Solved the major issue of not getting appropriate response. The two LLMs required specific prompts for effective response. Now, I have included these and the responses are much better.

--- constants.py
+++ constants.py
@@ -1,34 +1,45 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.llms import CTransformers
 
 CACHE_FOLDER_PATH ="cache/"
+
+# All PDFs within this path would be processed
 DATA_PATH = "data/"
+
 DB_PATH = "vectorDB/"
+
+
+
+#Embeddings setting:
+#   change device = 'cuda' for gpu; or 'cpu' for using only CPU.
+EMBEDDINGS_BASE_MODEL = "BAAI/bge-large-en-v1.5"
+EMBEDDINGS  = HuggingFaceBgeEmbeddings(model_name=EMBEDDINGS_BASE_MODEL, model_kwargs={'device': 'cuda'}, encode_kwargs={"normalize_embeddings": True}, cache_folder= CACHE_FOLDER_PATH)
+
+
+## Retrieval settings:
+# prompt suggested by EMBEDDINGS_BASE_MODEL: https://huggingface.co/BAAI/bge-large-en-v1.5
+RETRIEVAL_PROMPT = "Represent this sentence for searching relevant passages: "
 NUM_RETRIEVAL = 4
 
-#GPU changes:
-#Embeddings:
-#   change device = 'cuda' for gpu
-#CTRANSFORMERS_CONFIG
-#   'gpu_layers': 50
 
-EMBEDDINGS  = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'}, encode_kwargs={"normalize_embeddings": False}, cache_folder= CACHE_FOLDER_PATH)
-
-CTRANSFORMERS_CONFIG = {'max_new_tokens': 256, 'gpu_layers': 0, 'temperature': 0.01, 'top_p': 0.4, 'repetition_penalty': 1.1, 'context_length': 3500}
+#LLM settings and configuration
+#   'gpu_layers': 50 - if GPU is available - more layer eqals more parallelisation, but cannot be too high i.e. more than its capacity.
+#   'temperature': near zero - to force LLM to base its response on the context given.
+CTRANSFORMERS_CONFIG = {'max_new_tokens': 512, 'gpu_layers': 50, 'temperature': 0.01, 'top_p': 0.4, 'repetition_penalty': 1.1, 'context_length': 8500}
 
 LLM = CTransformers(
-        model = "tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",
+        model = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf",
         model_type="llama",
         config = CTRANSFORMERS_CONFIG
     )
 
-#prompt = hub.pull("rlm/rag-prompt")
-#"You are an assistant for question-answering tasks. 
-#Use the following pieces of retrieved context to answer the question. 
-#If you don't know the answer, just say that you don't know. 
-#Use three sentences maximum and keep the answer concise.\nQuestion: {question} \nContext: {context} \nAnswer:
-CUSTOM_PROMPT_TEMPLATE = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question.
-                        If you don't know the answer, just say that you don't know. 
-                        Keep the answer short and concise. 
-                        Add the original sources from the given Context at the end of your answer.
-                        \nThe only Question for you is: {question} \nContext: {context} \nThe Answer:"""
+
+# Prompts are LLM specific. So, if LLM is changed, then prompt may also has to be changed.
+# This prompt is for LLM: https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF
+CUSTOM_PROMPT_TEMPLATE = """
+<|system|>
+You are a helpful assistant for question-answering tasks based on given context only. If you don't know the answer, just say that you don't know. Keep the answer concise. You have to use the contexts given by the user to answer the question.
+<|user|>
+ The Context: {context}.\n The Question: {question}.
+ <|assistant|>
+ """
