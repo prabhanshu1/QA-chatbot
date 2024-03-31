479ec3c2c1d4c3935824136a18b595f361b54c4e
Prabhanshu Abhishek <prabhanshua@gmail.com>
2024-03-31 18:16:32 +0530

    1. cleaning the PDF documents before ingesting into the vectorDB, earlier cleaning was done after retrieval. This change will increase the accuracy of the similarity search and tokenisation by properly splitting the document. 2. changed the pdfLoader to PyPDFLoader and updated the separators list for chunking 3. Commented the functions and other codes.

--- dataIngestion.py
+++ dataIngestion.py
@@ -1,37 +1,52 @@
 import constants
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter 
 import time
+import re
 
+# Function to clean PDF documents by replacing tabs, removing hyphens at the end of lines,
+# replacing single newlines with spaces, and reducing multiple newlines to two newlines
+def cleanPDFDocuments(documents):
+    for doc in documents:
+        doc = doc.page_content.replace('\t', ' ')
+        doc = re.sub(r"(\w+)-\n(\w+)", r"\1\2", doc) # remove hyphenated words that are split across lines
+        doc = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", doc.strip()) # replace single newlines with spaces
+        doc = re.sub(r"\n\s*\n", "\n\n", doc) #  reduce newline followed by spaces and newline to two newlines
+    return documents
 
+# Function to process PDF data by loading PDF files from a directory, cleaning the documents,
+# and splitting the cleaned documents into chunks of text
 def processPDFData():
     loader = DirectoryLoader(constants.DATA_PATH,
                              glob='*.pdf',
-                             loader_cls=PyPDFLoader) ## try single mode 
+                             loader_cls=PyPDFLoader) 
     documents = loader.load()
+    cleaned_documents = cleanPDFDocuments(documents) #cleaning PDF before ingestion so that similarity search could be effective
     
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=750,
-                                                   chunk_overlap=100) # add custom separator \n \n\n . Also Tokeniser
+    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+                                                    chunk_size=2000,
+                                                    chunk_overlap=100)
 
-    texts = text_splitter.split_documents(documents)
+    texts = text_splitter.split_documents(cleaned_documents)
     return texts
 
-
+# Function to persist the processed texts to a vector database
 def persistTexts(texts):
     db = FAISS.from_documents(texts, constants.EMBEDDINGS)
     db.save_local(constants.DB_PATH)
 
 
-
+# Main function that processes and persists the texts from PDF files
+# Has to be run to process new PDF files;
+# In case of deletion or updation of the PDFs, first delete the vectorStore.
 if __name__ == "__main__":
-    t_start = time.perf_counter() #Timer start
+    t_start = time.perf_counter() #Timer for processing PDF
     texts = processPDFData()
-    t_process = time.perf_counter()  #Timer ends
-    print(f"Total time taken: {t_process - t_start:0.2f} seconds")
+    t_process = time.perf_counter()
+    print(f"Total time taken to process PDFs: {t_process - t_start:0.2f} seconds")
     
     ## Persisting the processed texts to vectorDB
     persistTexts(texts)
-    t_persist = time.perf_counter()  #Timer ends
-    print(f"Total time taken: {t_persist - t_process:0.2f} seconds")
+    t_persist = time.perf_counter()  
+    print(f"Total time taken to process and persist all the PDFs: {t_persist - t_process:0.2f} seconds")
--- responseGenerator.py
+++ responseGenerator.py
@@ -9,14 +9,19 @@ from langchain.prompts import PromptTemplate
 from langchain.schema.runnable.config import RunnableConfig
 import time
 
-
+# Creates a custom prompt template in the required format for the LLM to generate output based on context
 def getCustomPromptTemplate():
     prompt = PromptTemplate.from_template(constants.CUSTOM_PROMPT_TEMPLATE)
     return prompt
+# Adds Retrieval prompt at the start of the query as necessiated by the EMBEDDING LLM.
+def format_query(query):
+    return constants.RETRIEVAL_PROMPT + query
 
 def format_docs(docs):
     return "\n\n".join(f"{doc.page_content} \n {doc.metadata}" for doc in docs)
 
+# Creates a runnable chain for the language model by setting up a FAISS database retriever,
+# and a prompt template, and then chaining them together with the LLM
 def getRunnableChain():
     db = FAISS.load_local(constants.DB_PATH, constants.EMBEDDINGS)
     retriever = db.as_retriever(search_type="mmr", search_kwargs={'k': constants.NUM_RETRIEVAL, 'score_threshold': 0.8})
@@ -28,7 +33,7 @@ def getRunnableChain():
     chain = setup_and_retrieval | getCustomPromptTemplate() | constants.LLM | StrOutputParser()
     return chain
 
-
+# Function necessary to run the script from command line.
 if __name__ == "__main__":
     t_start = time.perf_counter() #Timer start
     chain = getRunnableChain()
@@ -40,14 +45,19 @@ if __name__ == "__main__":
     t_end = time.perf_counter()  #Timer ends
     print(f"Total time taken: {t_end - t_start:0.2f} seconds")
 
+## Chainlit module for Front-end and streaming response
 
-
+# defines callback functions for the chainlit module when the user starts a chat session.
+# it stores the user_session so that history of the chat could be used in subsequent interaction.
 @cl.on_chat_start
 async def on_chat_start():
     print("on_chat_start")
     cl.user_session.set("runnable", getRunnableChain())
     print("on_chat_start2")
 
+# called when the user sends a message. 
+# It retrieves the runnable chain from the user session object, and uses it to create response.
+# The response are then streamed to the user.
 @cl.on_message
 async def on_message(message: cl.Message):
     runnable = cl.user_session.get("runnable")  # type: Runnable
